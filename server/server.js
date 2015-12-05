25c71fda91ae0190230223ce992367352c8dbcb4
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2015-12-05 23:59:08 +0100

    first steps with requireJS

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = new express();
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/client/app/index.html');
-});
+//app.get('/', function(req, res) {
+//	res.sendFile(__dirname + '/client/app/index.html');
+//});
 
-app.use(express.static('client'));
+console.log(__dirname + '../client/app');
+
+app.use(express.static('../' + __dirname + 'client/app'));
+app.use(express.static('../' + __dirname + 'client/media/graphics'));
+
 
 app.listen(3000, function() {
 	console.log("server up and running..");
